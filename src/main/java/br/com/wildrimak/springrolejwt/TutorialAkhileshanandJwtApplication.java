5e659170c3cd5cee6ed4388d90782af3b21a49c3
Wildrimak <finalwildrimak@gmail.com>
2021-09-10 21:55:42 -0300

    Project started

---
+++ src/main/java/br/com/wildrimak/springrolejwt/TutorialAkhileshanandJwtApplication.java
@@ -0,0 +1,13 @@
+package br.com.wildrimak.springrolejwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialAkhileshanandJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialAkhileshanandJwtApplication.class, args);
+	}
+
+}
